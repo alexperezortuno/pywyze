c78e1d19f8c8da6766d0c3425ad414e076e21bc9
alexperezortuno <alexperezortuno@gmail.com>
2023-05-17 23:08:44 -0400

    remove unused options

--- wyze/core/logger.py
+++ wyze/core/logger.py
@@ -3,14 +3,9 @@
 import logging
 import coloredlogs
 
-from wyze.core.commons import log_lvl, log_str
-
 
 def get_logger(log_level: str, log_format: str, name: str = None) -> logging.Logger:
     res = logging.getLogger(__name__) if name is None else logging.getLogger(name)
     coloredlogs.install(level=log_level.upper(), fmt=log_format)
     return res
 
-
-if __name__ == '__main__':
-    get_logger(log_lvl, log_str)
